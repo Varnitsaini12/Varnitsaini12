06ad979a9bab989369d6317201be5e4527901846
Varnit Saini <varnitsaini2222@gamil.comm>
2022-11-18 11:36:41 +0530

    TT

---
+++ TT/oops_2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Employee{
+    public:
+        int id;
+        string name;
+        int no_of_leaves;
+    
+    void display(){
+        cin>>id>>name>>no_of_leaves;
+        cout<<"Employee Id: "<<id<<endl;
+        cout<<"Employee Name: "<<name<<endl;
+        cout<<"No of leaves: "<<no_of_leaves<<endl;
+    }
+};
+
+int main(){
+    Employee E1,E2;
+    // E1.id = 1; 
+    // E1.name = "Varnit";  
+    // E1.no_of_leaves = 2;
+    E1.display();
+    // E2.display();
+}
