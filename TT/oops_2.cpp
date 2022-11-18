1f985662c5364f960487e07fea7acaa5a16eaa31
Varnit Saini <varnitsaini2222@gamil.comm>
2022-11-18 12:14:41 +0530

    TT

--- TT/oops_2.cpp
+++ TT/oops_2.cpp
@@ -5,22 +5,28 @@ class Employee{
     public:
         int id;
         string name;
-        int no_of_leaves;
+        static int no_of_leaves;  //Class variable
     
     void display(){
         cin>>id>>name;
         cout<<"Employee Id: "<<id<<endl;
         cout<<"Employee Name: "<<name<<endl;
-        cout<<"No of leaves: "<<no_of_leaves<<endl;
+        cout<<"No of leaves: "<<no_of_leaves<<endl;  
     }
 };
 
+int Employee::no_of_leaves=50;
+
 int main(){
-    Employee E1,E2;
-    E1.no_of_leaves = 20;
+    Employee E1,E2,E3;
+    Employee::no_of_leaves = 60;
+    // E1.no_of_leaves = 30;   Do not use this method (Not Preferable)
     // E1.id = 1; 
     // E1.name = "Varnit";  
     // E1.no_of_leaves = 2;
     E1.display();
-    // E2.display();
+    // E2.no_of_leaves = 50;
+    E2.display();
+    // E3.no_of_leaves = 50;
+    E3.display();
 }
